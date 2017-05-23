ff4e008f741a3fd2cadd7da3f0ea99e06f9a1984
Frank <lee0324@126.com>
2017-05-23 23:14:13 +0800

    conf

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ module.exports = {
     port: '9993',
     ROUTE_BASE_PATH: '/applet',
 
-    
+    cosAppId: '填写开通 COS 时分配的 APP ID',
+    cosSecretId: '填写密钥 SecretID',
+    cosSecretKey: '填写密钥 SecretKey',
+    cosFileBucket: '填写创建的公有读私有写的bucket名称'
 };

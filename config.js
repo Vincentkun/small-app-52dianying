e796de81d85262505a870f83bdbcaa4f420b64c9
Frank <lee0324@126.com>
2017-05-25 14:26:56 +0800

    :wrench:

--- config.js
+++ config.js
@@ -1,10 +1,25 @@
-module.exports = {
-    // Node 监听的端口号
-    port: '9993',
-    ROUTE_BASE_PATH: '/applet',
+/**
+ * 小程序配置文件
+ */
 
-    cosAppId: '填写开通 COS 时分配的 APP ID',
-    cosSecretId: '填写密钥 SecretID',
-    cosSecretKey: '填写密钥 SecretKey',
-    cosFileBucket: '填写创建的公有读私有写的bucket名称'
+// 此处主机域名修改成腾讯云解决方案分配的域名
+var host = '15013558.frankcss.com';
+
+var config = {
+
+    // 下面的地址配合云端 Demo 工作
+    service: {
+        host,
+
+        // 登录地址，用于建立会话
+        loginUrl: `https://${host}/login`,
+
+        // 测试的请求地址，用于测试会话
+        requestUrl: `https://${host}/user`,
+
+        // 测试的信道服务地址
+        tunnelUrl: `https://${host}/tunnel`,
+    }
 };
+
+module.exports = config;

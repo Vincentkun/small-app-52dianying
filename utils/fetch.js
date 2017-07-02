5b11ca6700899b3b05437056c7eea058d8ddbed0
Frank <lee0324@126.com>
2017-07-02 17:02:35 +0800

    :bulb: request url

--- utils/fetch.js
+++ utils/fetch.js
@@ -4,30 +4,30 @@
  * fail 失败的回调
  */
 
-var pubURL = "https://api.douban.com/v2/movie/";
+var pubURL = 'https://api.douban.com/v2/movie/';
 
 function _get(category, start, success, fail) {
-  var start = start || 0;
-  var count = 20;
-  var apiUrl = pubURL + category + "?start=" + start + "&count=" + count;
-  console.log(category,start,count)
-  wx.request({
-    url: apiUrl,
-    header: {
-      "Content-Type": "json"
-    },
-    method: 'GET',
-    success: function (res) {
-      success(res);
-      console.log(res)
-    },
-    fail: function (res) {
-      fail(res);
-    }
-  });
+	var start = start || 0;
+	var count = 20;
+	var apiUrl = pubURL + category + '?start=' + start + '&count=' + count;
+	console.log(category, start, count);
+	wx.request({
+		url: apiUrl,
+		header: {
+			// 'Content-Type': 'application/json'
+			'Content-Type': 'json'
+		},
+		method: 'GET',
+		success: function(res) {
+			success(res);
+			console.log(res);
+		},
+		fail: function(res) {
+			fail(res);
+		}
+	});
 }
-
 
 module.exports = {
-    _get: _get
-}
+	_get: _get
+};
